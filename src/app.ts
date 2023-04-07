4a180bd38646db5ee137efdb32f9eae5c8b1b5c9
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-04-07 19:19:17 -0300

    Feature: PATCH /movies/:id

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, {Application, json} from "express";
 import { startDatabase } from "./database";
-import { catchMovieById, createMovie, listMovies } from "./logic";
+import { catchMovieById, createMovie, listMovies, updateMovie } from "./logic";
 import { ensureMovieExistsMiddleware } from "./middlewares";
 
 const app: Application = express();
@@ -12,7 +12,7 @@ const runningMsg: string = `Server running on http://localhost:${PORT}`;
 app.post("/movies", createMovie);
 app.get("/movies", listMovies);
 app.get("/movies/:id",ensureMovieExistsMiddleware , catchMovieById);
-app.patch("/movies/:id", ensureMovieExistsMiddleware, /* middlewares & logic */ );
+app.patch("/movies/:id", ensureMovieExistsMiddleware, updateMovie);
 app.delete("/movies/:id", ensureMovieExistsMiddleware, /* middlewares & logic */);
 
 app.listen(PORT, async() => {
--- src/logic.ts
+++ src/logic.ts
@@ -2,6 +2,7 @@ import { client } from "./database";
 import { Request, Response } from "express";
 import { Imovie } from "./interfaces";
 import { QueryConfig, QueryResult } from "pg";
+import format from "pg-format";
 
 export const createMovie = async (
     req: Request, res: Response
@@ -51,4 +52,34 @@ export const catchMovieById = async (
     const movie: Imovie = res.locals.movie;
 
     return res.json(movie);
+};
+
+export const updateMovie =  async (
+    req: Request,
+    res: Response
+): Promise<Response>=> {
+    const movieData: Partial<Imovie> = req.body;
+    const id: number = parseInt(req.params.id);
+
+    const queryString: string = format(
+        `
+        UPDATE
+            movies
+            SET(%I) = ROW(%L)
+        WHERE
+            id = $1
+        RETURNING *;
+        `,
+        Object.keys(movieData),
+        Object.values(movieData)
+    );
+
+    const queryConfig: QueryConfig = {
+        text: queryString,
+        values: [id],
+    };
+
+    const queryResult: QueryResult<Imovie> = await client.query(queryConfig);
+
+    return res.json(queryResult.rows[0]);
 };
